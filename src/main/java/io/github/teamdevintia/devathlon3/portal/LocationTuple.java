b7b2b81be9e7d271a1711068c71f874fc8d197ca
MiniDigger <admin@minidigger.me>
2016-07-16 14:54:19 +0200

    make location tuple iterable

--- src/main/java/io/github/teamdevintia/devathlon3/portal/LocationTuple.java
+++ src/main/java/io/github/teamdevintia/devathlon3/portal/LocationTuple.java
@@ -3,12 +3,14 @@ package io.github.teamdevintia.devathlon3.portal;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 
+import java.util.Iterator;
+
 /**
  * A small tuple holding two locations
  *
  * @author MiniDigger
  */
-public class LocationTuple {
+public class LocationTuple implements Iterable<Location> {
 
     public Location loc1;
     public Location loc2;
@@ -26,5 +28,33 @@ public class LocationTuple {
 
         LocationTuple tuple = (LocationTuple) obj;
         return ( loc1.equals( tuple.loc1 ) || loc1.equals( tuple.loc2 ) ) && ( loc2.equals( tuple.loc1 ) || loc2.equals( tuple.loc2 ) );
+    }
+
+    @Override
+    public Iterator<Location> iterator() {
+        Iterator<Location> it = new Iterator<Location>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < 2;
+            }
+
+            @Override
+            public Location next() {
+                currentIndex++;
+                if ( currentIndex == 1 ) {
+                    return loc1;
+                } else {
+                    return loc2;
+                }
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
     }
 }
