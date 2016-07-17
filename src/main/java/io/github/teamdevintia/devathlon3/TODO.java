64470df47b4d2a9ae1199c3f946bd2906399abd4
MiniDigger <admin@minidigger.me>
2016-07-17 02:16:27 +0200

    moar todos

--- src/main/java/io/github/teamdevintia/devathlon3/TODO.java
+++ src/main/java/io/github/teamdevintia/devathlon3/TODO.java
@@ -5,5 +5,15 @@ package io.github.teamdevintia.devathlon3;
  */
 public class TODO {
 
+    // TODO eis potion
+    // TODO implus potion
+    // TODO zeit stop potion
+    // TODO sonnen potion
+    // TODO winter potion
+
+    // TODO buch vom zauberer
+
+    // TODO neue hexen mechanic
+
     // TODO plugin.yml aufbessern
 }
